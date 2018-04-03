e3aefaade2e501626b01d478560a10464a21226b
david <davidtnfsh@gmail.com>
2018-04-03 22:49:48 +0800

    almost finish

--- Pattern Recognition/knn.py
+++ Pattern Recognition/knn.py
@@ -1,4 +1,5 @@
 import scipy, pandas, numpy
+import numpy as np
 from collections import Counter
 from sklearn.preprocessing import Normalizer
 from sklearn.cross_validation import KFold
@@ -15,6 +16,7 @@ class KNN(object):
 		self.Y = array[:,0]
 		# normalizer can turn length of vector into 1.
 		scaler = Normalizer().fit(self.X)
+
 		self.data = scaler.transform(self.X)
 		numpy.set_printoptions(precision=3)
 		# print(self.data[0:5,:])
@@ -28,23 +30,27 @@ class KNN(object):
 		:return: class
 		"""
 		# 計算每個訓練集合特徵關鍵字字詞頻率向量和輸入向量的距離
-		vec_distance = dict()
+		result = []
 		for input_vec in testset_vec:
+			vec_distance = dict()
 			for v, c in zip(trainset_vec, trainset_class):
-				vec_distance[1 - scipy.spatial.distance.cosine(input_vec, v)] = c
-			yield max(Counter([c for sim, c in sorted(vec_distance.items())[:self.k]]).items(), key=lambda x:x[1])[0]
+				euclidean_distance = np.linalg.norm(np.array(input_vec)-np.array(v))
+				vec_distance[euclidean_distance] = c
+			result.append(max(Counter([c for distance, c in sorted(vec_distance.items(), key=lambda x:x[0])[:self.k]]).items(), key=lambda x:x[1])[0])
+		return result
 
 	def evaluate(self):
-		folds = 2
-		kf = KFold(len(self.data), n_folds=folds)
+		folds, itertimes = 2, 20
 		accuracy = 0
-		for train_index, test_index in kf:
-		    X_train, X_test = self.X[train_index], self.X[test_index]
-		    y_train, y_test = self.Y[train_index], self.Y[test_index]
-		    predict = self.classify(X_test, X_train, y_train)
-		    accuracy += len([1 for i,j in zip(predict, y_test) if i==j]) / len(y_test)
-		print(accuracy, accuracy / folds, len(kf), 	folds)
+		for _ in range(itertimes):
+			kf = KFold(len(self.data), n_folds=folds, shuffle=True)
+			for train_index, test_index in kf:
+				X_train, X_test = self.X[train_index], self.X[test_index]
+				y_train, y_test = self.Y[train_index], self.Y[test_index]
+				predict = self.classify(X_test, X_train, y_train)
+				accuracy += len([1 for i,j in zip(predict, y_test) if i==j]) / len(y_test)
+		print(accuracy / folds / itertimes)
 
 if __name__ == '__main__':
-    k = KNN()
-    k.evaluate()
+	k = KNN()
+	k.evaluate()
